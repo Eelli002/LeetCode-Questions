744e20b9bcdfdcfd2689319b661835a0d7038b95
Elijah Elliott <70392722+Eelli002@users.noreply.github.com>
2022-06-27 11:58:48 -0700

    Time: 134 ms (5.05%), Space: 14.3 MB (14.74%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.py
@@ -1,25 +1,43 @@
-class Solution:
-    def reverseWords(self, s: str) -> str:
-        str_list = []
-        curr_word = ""
-        
-        for i in range(len(s)):
-            if s[i] != ' ':
-                curr_word += s[i]
-                
-            elif len(curr_word) != 0:
-                str_list.append(curr_word)
-                curr_word = ""
-                
-            else: curr_word = ""
-        if len(curr_word) > 0: str_list.append(curr_word)
-        
-        reversed_str = str_list[len(str_list)-1]
-        for i in range(len(str_list)-2, -1, -1):
-            reversed_str += " " + str_list[i]
-            
-        print(str_list)
-            
-        return reversed_str
-        
-        
+class Solution(object):
+    def reverseWords(self, s):
+        arr = list(s)
+        self.reverse_string(arr, 0, len(arr)-1)
+        self.reverse_word(arr)
+        word = self.trim_sides(arr)
+        res = self.trim_space(word)
+        return ''.join(res)
+
+
+    def reverse_string(self, arr, l, r):
+        '''reverse a given string'''
+        while l < r:
+            arr[l], arr[r] = arr[r], arr[l]
+            l += 1 ; r -= 1
+        return arr
+    
+    
+    def reverse_word(self, arr):
+        '''reverse every words in a string'''
+        l, r = 0 , 0
+        while r < len(arr):
+            while r < len(arr) and not arr[r].isspace(): r += 1
+            self.reverse_string(arr, l, r-1)
+            r += 1; l = r
+        return arr
+    
+    def trim_sides(self, arr):
+        '''str.strip() basically'''
+        if ''.join(arr).isspace(): return []
+        l , r = 0, len(arr) - 1
+        while l < r and arr[l].isspace(): l += 1
+        while l < r and arr[r].isspace(): r -= 1
+        return arr[l:r+1]
+    
+    def trim_space(self, word):
+        '''remove duplicating space in a word'''
+        if not word: return []
+        res = [word[0]]            
+        for i in range(1, len(word)):
+            if res[-1].isspace() and word[i].isspace(): continue
+            res.append(word[i])
+        return res
