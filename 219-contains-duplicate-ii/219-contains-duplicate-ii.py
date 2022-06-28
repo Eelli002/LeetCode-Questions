37fec50c347a64ad01533f4067dd242786f0257b
Elijah Elliott <70392722+Eelli002@users.noreply.github.com>
2022-06-28 09:56:16 -0700

    Time: 1093 ms (20.37%), Space: 25.4 MB (96.25%) - LeetHub

---
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        window_map = {}
+        start = 0
+        window_map[nums[start]] = 1
+        for end in range(1, len(nums)):
+            if abs(start - end) > k:
+                window_map.pop(nums[start])
+                start += 1
+                # if nums[start] in window_map: return True
+                
+            if nums[end] in window_map: return True
+            else: window_map[nums[end]] = 1
+        return False
