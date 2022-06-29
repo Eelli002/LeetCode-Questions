b87bd7e6efefb78e5777c6f77bd11c14af80cfe7
Elijah Elliott <70392722+Eelli002@users.noreply.github.com>
2022-06-29 07:51:41 -0700

    Time: 289 ms (39.80%), Space: 14 MB (32.42%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -4,17 +4,16 @@ class Solution:
         
         for i in range(len(s)):
             start = end = i
-            while start >= 0 and end < len(s) and s[start] == s[end]:
+            while start >=0 and end < len(s) and s[start] == s[end]:
                 count += 1
-                start -= 1
-                end += 1
+                start -=1
+                end +=1
             
             start = i
-            end = i + 1
-            while start >= 0 and end < len(s) and s[start] == s[end]:
+            end = i+1
+            while start >=0 and end < len(s) and s[start] == s[end]:
                 count += 1
-                start -= 1
-                end += 1
-                
+                start -=1
+                end +=1
+        
         return count
-            
