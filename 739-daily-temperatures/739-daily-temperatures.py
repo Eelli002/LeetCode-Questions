3caa7010d1d33fc593910c21b721dd5c32881361
Elijah Elliott <70392722+Eelli002@users.noreply.github.com>
2022-07-01 23:38:06 -0700

    Time: 1914 ms (33.55%), Space: 24.9 MB (62.93%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.py
+++ 739-daily-temperatures/739-daily-temperatures.py
@@ -4,28 +4,9 @@ class Solution:
         stack = []
         answer = [0] * n
         
-        
-        for i, v in enumerate(temperatures):
-            while stack and v > stack[-1][0]:
+        for idx, val in enumerate(temperatures):
+            while stack and val > stack[-1][0]:
                 stackVal, stackIdx = stack.pop()
-                answer[stackIdx] = i - stackIdx
-            stack.append([v, i])
-        return answer
-                
-            
-        
-        
-        
-        return answer
-#         for i in range(n):
-#             answer.append(0)
-#             if len(t_stack) == 0: t_stack.append(temperatures[i])
-#             else:
-#                 while t_stack and temperatures[i] > t_stack[len(t_stack)-1]:
-#                     index = temperatures.index(t_stack[len(t_stack)-1])
-#                     answer[index] = i - index - 1
-#                     t_stack.pop()
-#                 t_stack.append(temperatures[i])
-            
-#         return answer
-        
+                answer[stackIdx] = idx - stackIdx
+            stack.append([val, idx])
+        return answer     
