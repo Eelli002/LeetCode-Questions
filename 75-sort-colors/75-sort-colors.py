4bb6f717f404b54e0afe455dca84116a06ab0f50
Elijah Elliott <70392722+Eelli002@users.noreply.github.com>
2022-07-03 21:40:08 -0700

    Time: 69 ms (13.03%), Space: 14 MB (15.06%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -4,17 +4,17 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         start = 0
+        idx = 0
         end = len(nums)-1
-        index = 0
         
-        while index <= end:
-            if nums[index] == 0:
-                nums[index] = nums[start]
+        while idx <= end and start < end:
+            if nums[idx] == 0:
+                nums[idx] = nums[start]
                 nums[start] = 0
                 start += 1
-                index += 1
-            elif nums[index] == 2:
-                nums[index] = nums[end]
+                idx += 1
+            elif nums[idx] == 2:
+                nums[idx] = nums[end]
                 nums[end] = 2
                 end -= 1
-            else: index += 1
+            else: idx += 1
