11219c5bcf4f9b6e35766c607b6426670cacc710
Elijah Elliott <70392722+Eelli002@users.noreply.github.com>
2022-07-09 20:02:26 -0700

    Time: 42 ms (91.08%), Space: 13.9 MB (89.43%) - LeetHub

---
+++ 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.py
@@ -0,0 +1,17 @@
+class Solution:
+    def helper(self, s:str, start:int, end:int, k:int):
+        if end-start < k: return 0
+
+        s_table = Counter(s[start:end])
+        for i in range(start, end):
+            if s_table[s[i]] < k:
+                j = i+1
+                while j < len(s) and s_table[s[j]] < k:
+                    j+=1
+                return max(self.helper(s, 0, i, k), self.helper(s, j, end, k))
+        return end-start
+                
+                
+    
+    def longestSubstring(self, s: str, k: int) -> int:
+        return self.helper(s, 0, len(s), k)
