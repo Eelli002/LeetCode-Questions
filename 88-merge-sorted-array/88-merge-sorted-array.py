46da2ba08aa9b82065aa1349b931221cef7fe015
Elijah Elliott <70392722+Eelli002@users.noreply.github.com>
2022-07-09 19:34:51 -0700

    Time: 44 ms (81.47%), Space: 13.9 MB (37.68%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -3,17 +3,18 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        index = len(nums1)-1
+        swapper = len(nums1)-1
+        
         while m > 0 and n > 0:
             if nums1[m-1] > nums2[n-1]:
-                nums1[index] = nums1[m-1]
+                nums1[swapper] = nums1[m-1]
                 m -= 1
             else:
-                nums1[index] = nums2[n-1]
-                n -=1
-            index -= 1
+                nums1[swapper] = nums2[n-1]
+                n -= 1
+            swapper -= 1
+            
         while n > 0:
-            nums1[index] = nums2[n-1]
-            n -=1
-            index -= 1
-                
+            nums1[swapper] = nums2[n-1]
+            n-=1
+            swapper-=1
