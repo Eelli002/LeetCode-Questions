696bb879e11e4ffa3eb584204110e074f7f4c1f2
Elijah Elliott <70392722+Eelli002@users.noreply.github.com>
2022-09-10 15:09:04 -0700

    Time: 77 ms (14.40%), Space: 13.9 MB (79.50%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -6,26 +6,24 @@
 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-
-        if list1 == None and list2 == None: return None
-        elif list1 == None: return list2
-        elif list2 == None: return list1
-        else:
-            newList = ListNode(None)
-            new_list_head = newList
-
-            while list1 and list2:
-                if list1.val < list2.val:
-                    newList.next = ListNode(list1.val)
-                    newList = newList.next
-                    list1 = list1.next
-                else:
-                    newList.next = ListNode(list2.val)
-                    newList = newList.next
-                    list2 = list2.next
-
-            if list1: newList.next = list1
-            else: newList.next = list2
-
-            new_list_head = new_list_head.next
-            return new_list_head
+        new_list_head = ListNode()
+        new_list_tail = new_list_head
+        
+        while list1 and list2:
+            if list1.val < list2.val:
+                new_list_tail.next = list1
+                list1 = list1.next
+            else:
+                new_list_tail.next = list2
+                list2 = list2.next
+            new_list_tail = new_list_tail.next
+            
+        if list1:
+            new_list_tail.next = list1
+            new_list_tail = new_list_tail.next
+        elif list2:
+            new_list_tail.next = list2
+            new_list_tail = new_list_tail.next
+        new_list_head = new_list_head.next
+        return new_list_head
+            
