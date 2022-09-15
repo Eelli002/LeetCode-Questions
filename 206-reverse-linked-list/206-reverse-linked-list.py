b79089ef0b3d584e4457cc520cca848efa36139d
Elijah Elliott <70392722+Eelli002@users.noreply.github.com>
2022-09-15 09:49:43 -0700

    Time: 72 ms (20.64%), Space: 15.4 MB (55.53%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -4,29 +4,11 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def Get_Tail(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        tail = head
-        while head:
-            tail = head
-            head = head.next
-        return tail
-        
-    
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head: return head
-        reversed_head = ListNode(None)
-        reversed_pointer = reversed_head
-        list_pointer = head
-        tail = self.Get_Tail(head)
-        while head.next:
-            while list_pointer.next != tail:
-                list_pointer = list_pointer.next
-            reversed_pointer.next = tail
-            reversed_pointer = reversed_pointer.next
-            tail = list_pointer
-            tail.next = None
-            list_pointer = head
-        
-        reversed_pointer.next = head
-        reversed_head = reversed_head.next
-        return reversed_head
+        prev, curr = None, head
+        while curr:
+            temp_pointer = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp_pointer
+        return prev
