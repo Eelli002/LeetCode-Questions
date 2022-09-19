ecd4c8d663d555f430cb61a29158eac97f3802f5
Elijah Elliott <70392722+Eelli002@users.noreply.github.com>
2022-09-19 15:52:30 -0700

    Time: 46 ms (62.87%), Space: 13.9 MB (74.49%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.py
+++ 844-backspace-string-compare/844-backspace-string-compare.py
@@ -1,15 +1,19 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        s_total = t_total = ''
+        s_total = []
+        t_total = []
+        
         
         for char in s:
             if char == '#':
-                s_total = s_total[:-1]
-            else: s_total = s_total + char
+                if s_total: s_total.pop()
+            else: s_total.append(char)
+        print(s_total)
                 
         for char in t:
             if char == '#':
-                t_total = t_total[:-1]
-            else: t_total = t_total + char
+                if t_total: t_total.pop()
+            else: t_total.append(char)
+        print(t_total)
                 
         return True if s_total == t_total else False
