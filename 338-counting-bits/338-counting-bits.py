5dafed3c31f31d0bf9de6e0560c323b1bc3c354a
Elijah Elliott <70392722+Eelli002@users.noreply.github.com>
2022-09-19 19:12:49 -0700

    Time: 541 ms (5.01%), Space: 20.8 MB (79.12%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        answer = []
+        for i in range(0, n+1, 1):
+            print(i)
+            one_count = 0
+            bin_str = str(bin(i))
+            for char in bin_str:
+                if char == '1': one_count += 1
+            answer.append(one_count)
+            
+        return answer
