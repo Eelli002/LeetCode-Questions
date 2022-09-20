776f697e6483edb6adbfcde06d41e3e75dc1c69c
Elijah Elliott <70392722+Eelli002@users.noreply.github.com>
2022-09-19 21:27:11 -0700

    Time: 107 ms (13.46%), Space: 14 MB (30.16%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,18 +1,12 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         numeral_map = { 'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000 }
-        special_map = ['I', 'X', 'C']
-        int_total = 0
-        
-        curr = checker = 0
+        int_total = curr = 0
+
         while curr < len(s):
-            if s[curr] in special_map and curr+1 < len(s):
-                if numeral_map[ s[curr+1] ] > numeral_map[s[curr]]:
-                    int_total += numeral_map[ s[curr+1] ] - numeral_map[s[curr]]
-                    curr += 2
-                else:
-                    int_total += numeral_map[s[curr]]
-                    curr += 1
+            if curr+1 < len(s) and numeral_map[s[curr]] < numeral_map[s[curr+1]]:
+                int_total += numeral_map[ s[curr+1] ] - numeral_map[s[curr]]
+                curr += 2
             else:
                 int_total += numeral_map[s[curr]]
                 curr += 1
