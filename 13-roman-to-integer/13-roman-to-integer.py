49b9b22a92c10cf210b3cc2d98e2e3a6be5972e9
Elijah Elliott <70392722+Eelli002@users.noreply.github.com>
2022-09-19 21:42:10 -0700

    Time: 118 ms (6.63%), Space: 13.9 MB (76.53%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -3,11 +3,9 @@ class Solution:
         numeral_map = { 'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000 }
         int_total = curr = 0
 
-        while curr < len(s):
+        for curr in range(len(s)):
             if curr+1 < len(s) and numeral_map[s[curr]] < numeral_map[s[curr+1]]:
-                int_total += numeral_map[ s[curr+1] ] - numeral_map[s[curr]]
-                curr += 2
+                int_total -= numeral_map[s[curr]]
             else:
                 int_total += numeral_map[s[curr]]
-                curr += 1
         return int_total
