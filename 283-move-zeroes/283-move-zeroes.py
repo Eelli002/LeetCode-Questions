9f9bcf18b307886bba1411bb4c5b297e73e9abca
Elijah Elliott <70392722+Eelli002@users.noreply.github.com>
2022-09-22 09:40:28 -0700

    Time: 374 ms (32.36%), Space: 15.6 MB (16.62%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -6,7 +6,5 @@ class Solution:
         l = 0
         for r in range(len(nums)):
             if nums[r] != 0:
-                temp = nums[l]
-                nums[l] = nums[r]
-                nums[r] = temp
+                nums[l], nums[r] = nums[r], nums[l]
                 l += 1
