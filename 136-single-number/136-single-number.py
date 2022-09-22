4f15348bfb85a0e72ceddd08cea6b4cda511e212
Elijah Elliott <70392722+Eelli002@users.noreply.github.com>
2022-09-21 20:42:38 -0700

    Time: 368 ms (16.46%), Space: 16.7 MB (49.54%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,5 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        count = Counter(nums)
-        for num in count:
-            if count[num] != 2: return num
+        result = 0
+        
+        for num in nums:
+            result = num ^ result
+        return result
